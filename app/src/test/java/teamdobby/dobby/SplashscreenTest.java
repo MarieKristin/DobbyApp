31225dd84a3771972e54e3483f988b839850298d
capetown-nico <n.huentz@gmx.de>
2016-05-12 13:24:45 +0200

    Coverage - 39

--- app/src/test/java/teamdobby/dobby/SplashscreenTest.java
+++ app/src/test/java/teamdobby/dobby/SplashscreenTest.java
@@ -1,4 +1,4 @@
-package teamdobby.dobby;
+/*package teamdobby.dobby;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -32,7 +32,7 @@ import static org.mockito.Mockito.*;
 
 /**
  * Created by Marie on 02.05.2016.
- */
+ *//*
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Splashscreen.class,AnimationUtils.class})
 public class SplashscreenTest {
@@ -77,10 +77,12 @@ public class SplashscreenTest {
 
         verify(animation).setAnimationListener((Animation.AnimationListener) Matchers.any());
     }*/
-
+/*
     @Test
     public void testOnBackPressed() {
         splashscreen.onBackPressed();
         assertTrue(true);
     }
 }
+
+*/
